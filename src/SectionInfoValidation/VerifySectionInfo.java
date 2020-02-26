042a347e1b914f763a4989d5bc9a7db39baf2bd1
UmutsDemo <61520204+UmutsDemo@users.noreply.github.com>
2020-02-26 14:47:09 -0800

    this is the updating version
    this commit includes upodates of Project1

---
+++ src/SectionInfoValidation/VerifySectionInfo.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package SectionInfoValidation;
+
+/**
+ * @author umutx
+ *
+ */
+public class VerifySectionInfo {
+
+	public void PreTest() {
+		
+		
+		
+		System.out.println("this is the demo for tests");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
